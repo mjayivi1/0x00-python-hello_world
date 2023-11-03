14c69ee7ac39ce667914064b75a090b0cbd4a317
Mike <64213533+mjayivi1@users.noreply.github.com>
2023-11-03 04:20:56 +0000

    Add files via upload

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    c = len(argv)
-    sum = 0
-    if c == 1:
-        print("{}".format(sum))
+    count = len(argv)
+    add = 0
+    if count == 1:
+        print("{}".format(add))
     else:
-        for i in range(1, c):
-            sum +=int(argv.__getitem__(i))
-    print("{}".format(sum))
+        for i in range(1, count):
+            add += int(argv.__getitem__(i))
+        print("{}".format(add))
