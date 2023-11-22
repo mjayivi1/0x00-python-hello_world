278d60aeb4a964e46839ed1e82bcd0144215a71c
Mike <64213533+mjayivi1@users.noreply.github.com>
2023-11-22 05:47:04 +0000

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines a square
+"""
+
+
 class Square:
+    """
+    Defines a square
+    """
     def __init__(self, size=0):
         self.__size = size
