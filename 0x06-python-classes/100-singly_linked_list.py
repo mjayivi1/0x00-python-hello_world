a072ef5c417da6fd38514360f65708e10d3a7570
Mike <64213533+mjayivi1@users.noreply.github.com>
2023-11-22 05:59:59 +0000

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,8 +1,62 @@
 #!/usr/bin/python3
+
 class Node:
     """Class Node that defines a node of a singly linked list"""
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
 class SinglyLinkedList:
+    """Class SinglyLinkedList that defines a singly linked list"""
+    def __init__(self):
+        self.head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.head is None or self.head.data >= value:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        result = ""
+        current = self.head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result.rstrip()
+
+# Test the SinglyLinkedList class
+if __name__ == "__main__":
+    singly_linked_list = SinglyLinkedList()
+    values_to_insert = [2, 5, 3, 10, 1, -4, -3, 4, 5, 12, 3]
+
+    for value in values_to_insert:
+        singly_linked_list.sorted_insert(value)
+
+    print(singly_linked_list)
