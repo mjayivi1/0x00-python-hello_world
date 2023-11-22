0c4d1532a6d13f14afda60e211b15d24e0c27a59
Mike <64213533+mjayivi1@users.noreply.github.com>
2023-11-22 05:53:34 +0000

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
+"""
+Module 4-square
+Defines a square
+"""
+
+
 class Square:
+    """
+    Defines a square
+    """
     def __init__(self, size=0):
         self.__size = size
-
-    def area(self):
-        return self.__size**2
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
+        """Getter method for size"""
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+    def size(self, value):
+        """Setter method for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
